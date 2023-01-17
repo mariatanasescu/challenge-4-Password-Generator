b8311a8988ee66d8ed2d80869cd6812542ab12bd
Maria Tanasescu <mariatanasescu23@gmail.com>
2023-01-17 17:52:11 +0000

    working java

--- script.js
+++ script.js
@@ -88,9 +88,22 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+let wantsUppercase 
+let wantsLowercase 
+let wantsNumeric 
+let wantsSpecial 
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  wantsUppercase = confirm ("Do you want Uppercase characters?")
+  wantsLowercase = confirm ("Do you want Lowercase characters?")
+  wantsNumeric = confirm ("Do you want Numeric characters?")
+  wantsSpecial = confirm ("Do you want Special characters?")
+  if (wantsUppercase === false && wantsLowercase === false && wantsNumeric === false && wantsSpecial === false) {
+    alert("You must choose at least one option");
+    getPasswordOptions()
+  }
+ 
 }
 
 // Function for getting a random element from an array
@@ -100,7 +113,33 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  let passwordLength = parseInt(prompt ("How long do you want your password to be?"))
+  if (passwordLength < 10||
+    passwordLength > 64) {
+      alert("Length sould be more than 10 but no more than 64 characters");
+      generatePassword()
+    } 
+ getPasswordOptions();  
+ let passwordOptions = []
+ if (wantsUppercase === true) {
+   passwordOptions = passwordOptions.concat(upperCasedCharacters)
+ }
+ if (wantsLowercase === true) {
+   passwordOptions = passwordOptions.concat(lowerCasedCharacters)
+ }
+ if (wantsNumeric === true) {
+   passwordOptions = passwordOptions.concat(numericCharacters)
+ }
+ if (wantsSpecial === true) {
+   passwordOptions = passwordOptions.concat(specialCharacters)
+ }
+ let password = ""
+ for ( let i = 0; i < passwordLength; i++) {
+  console.log(i)
+  let randomChar = passwordOptions[Math.floor(Math.random()*passwordOptions.length)]
+  password = password + randomChar
+ }
+ return password
 }
 
 // Get references to the #generate element
